4b42f33d9f3fb9fc8741e15f98ee0451a71e0c9b
ejjagiriramesh <ejjagiri.ramesh09@gmail.com>
2024-12-28 22:41:58 +0530

    MVC PROJECT

---
+++ src/main/java/com/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package com.controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("message", "Welcome to Spring MVC Application!");
+        return "home"; // Corresponds to home.jsp
+    }
+}
